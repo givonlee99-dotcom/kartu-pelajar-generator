e0760f3e5c21413c7a0e964fdf02c94eba0a28de
givonlee99-dotcom <givonlee99@gmail.com>
2026-04-22 16:56:18 +0800

    Upload kartu pelajar generator

---
+++ script.js
@@ -0,0 +1,100 @@
+let template = "";
+
+/* pilih template */
+
+function selectTemplate(img) {
+  template = img.src;
+
+  document.querySelectorAll(".template img").forEach((i) => {
+    i.style.border = "2px solid transparent";
+  });
+
+  img.style.border = "3px solid red";
+}
+
+/* preview kartu */
+
+function previewCard() {
+  document.getElementById("preview").style.display = "block";
+
+  document.getElementById("sekolahCard").innerText =
+    document.getElementById("sekolah").value;
+
+  document.getElementById("alamatCard").innerText =
+    document.getElementById("alamat").value;
+
+  document.getElementById("namaCard").innerText =
+    document.getElementById("nama").value;
+
+  document.getElementById("nisnCard").innerText =
+    document.getElementById("nisn").value;
+
+  document.getElementById("masaCard").innerText =
+    "Berlaku Sampai : " + document.getElementById("masa").value;
+
+  /* pasang template */
+
+  if (template) {
+    document.getElementById("card").style.backgroundImage =
+      "url('" + template + "')";
+    document.getElementById("card").style.backgroundSize = "cover";
+  }
+
+  /* QR CODE */
+
+  document.getElementById("qrCard").innerHTML = "";
+
+  new QRCode(document.getElementById("qrCard"), {
+    text: document.getElementById("nisn").value,
+    width: 60,
+    height: 60,
+  });
+
+  /* logo */
+
+  const logoFile = document.getElementById("logo").files[0];
+
+  if (logoFile) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      document.getElementById("logoCard").src = e.target.result;
+    };
+
+    reader.readAsDataURL(logoFile);
+  }
+
+  /* foto siswa */
+
+  const fotoFile = document.getElementById("foto").files[0];
+
+  if (fotoFile) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      document.getElementById("fotoCard").src = e.target.result;
+    };
+
+    reader.readAsDataURL(fotoFile);
+  }
+}
+
+/* download kartu */
+
+function downloadCard() {
+  html2canvas(document.getElementById("card")).then((canvas) => {
+    const link = document.createElement("a");
+
+    link.download = "kartu.png";
+
+    link.href = canvas.toDataURL();
+
+    link.click();
+  });
+
+  /* redirect linkvertise */
+
+  setTimeout(() => {
+    window.open("LINKVERTISE_KAMU", "_blank");
+  }, 1000);
+}
