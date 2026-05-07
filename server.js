12661c6cfb868837236a62ad8ac42d65538c2bfb
givonlee99-dotcom <givonlee99@gmail.com>
2026-05-07 09:59:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
-app.use("/assets", express.static("assets"));
-app.use("/uploads", express.static("uploads"));
+/* static file root */
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
